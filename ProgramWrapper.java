3124df7eff18bc1c6d761f414a747024929e9e99
kevgom018 <157429350+kevgom018@users.noreply.github.com>
2024-11-23 00:14:32 -0400

    Changed avoided bugs

--- ProgramWrapper.java
+++ ProgramWrapper.java
@@ -30,23 +30,23 @@ public class ProgramWrapper {
      * 
      * @return A boolean, true if the email is valid, false otherwise
      */
-    public boolean isValidEmail(String email){
+    public static boolean isValidEmail(String email){
         int indexOfDot = email.length() - 4;
         if(!(Character.isLetter(email.charAt(0)) && email.contains("@") && email.indexOf("@") < indexOfDot && email.charAt(indexOfDot) == '.')){
             return false;
         }
-
+        
         for(int i = 0; i < email.length(); i++){
             char c = email.charAt(i);
             if(!(Character.isLetterOrDigit(c) || c == '@' || c == '.')){
                 return false;
             }
         }
-
+        
         return true;
     }
- 
-
+    
+    
     /**
      * Confirms whether the given phone number is within a valid range
      * 
@@ -54,21 +54,21 @@ public class ProgramWrapper {
      * 
      * @return A boolean, true if the phone number is valid, false otherwise
      */
-    public boolean isValidPhoneNum(Long phone){
+    public static boolean isValidPhoneNum(Long phone){
         return Long.parseLong("9999999999") >= phone && phone >= 1000000000;
     }
-
+    
     /**
      * Asks user for Client info and makes a new Client variable with the info
      * 
      * @return A Client variable composed of the information given by the user
      */
-    public Client makeClient(){
+    public static Client makeClient(Scanner scan){
+        System.out.println("*****Enter client info*****");
         String name = "";
         String email = "";
         Long phone = Long.parseLong("123456789");
-        Scanner scan = new Scanner(System.in);
-
+        
         boolean valid = false;
         while(!valid){
             System.out.println("Enter name: ");
@@ -81,34 +81,42 @@ public class ProgramWrapper {
                 System.out.println();
             }
         }
-
+        
         valid = false;
         while(!valid){
             System.out.println("Enter email: ");
             try {
                 email = scan.next();
                 valid = isValidEmail(email);
+                if(!valid){
+                    System.out.println("Invalid Email.");
+                    System.out.println("Try again.");
+                    System.out.println();
+                }
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
                 System.out.println("Invalid Email.");
                 System.out.println();
             }
         }
-
+        
         valid = false;
         while(!valid){
             System.out.println("Enter phone number: ");
             try {
                 phone = scan.nextLong();
                 valid = isValidPhoneNum(phone);
+                if(!valid){
+                    System.out.println("Invalid phone number.");
+                    System.out.println("Try again.");
+                    System.out.println();
+                }
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
                 System.out.println("Invalid Phone Number.");
                 System.out.println();
             }
-        }
-        scan.close();
-
+        }        
         return new Client(name.toUpperCase(), email.toLowerCase(), phone);
     }
 
